04f8fb2dfa7dabc0f8877f0e8d946884125874ad
Troy Chryssos <github email address>
2016-07-26 19:02:18 -0400

    created a school domain which had a roster that could be added to, checked, and organized.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+
+class School
+
+    # ROSTER = {}
+
+    def initialize(name)
+      @name=name
+      @school_roster={}
+    end
+
+    def name
+      @name
+    end
+
+    def roster
+      @school_roster
+    end
+
+    def add_student(name, grade)
+      if @school_roster.has_key? (grade)
+        @school_roster[grade]<<name
+      else
+        @school_roster[grade]=[name]
+      end
+    end
+
+    def grade(grade)
+      @school_roster[grade]
+    end
+
+    def sort
+      @school_roster.each do |grade, students|
+        students.sort!
+      end
+    end
+end
